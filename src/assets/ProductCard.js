40e2f227bf5a3acde3d1ea43141efe7eb0dfee47
Evan Harrison <esharri2@gmail.com>
2025-01-25 18:52:16 -0500

    add swatch change functionality

--- src/assets/ProductCard.js
+++ src/assets/ProductCard.js
@@ -4,12 +4,56 @@ export class ProductCard extends HTMLElement {
   }
 
   connectedCallback() {
-    console.log("connected!");
-    /* Init functions */
     this.bindEvents();
   }
 
-  bindEvents() {}
+  bindEvents() {
+    this.addEventListener("click", ({ target }) => {
+      if (target.matches("[data-variant-switch]:not([aria-pressed='true'])")) {
+        this.handleSwatchClick(target);
+      }
+    });
+  }
+
+  handleSwatchClick(target) {
+    const { variantId, variantPrice, variantCompareAtPrice } = target.dataset;
+
+    // Update images
+    this.images = this.images || this.querySelectorAll(`img[data-variant-id]`);
+
+    this.images.forEach((image) => {
+      image.classList.add("hidden");
+
+      if (image.dataset.variantId === variantId) {
+        image.classList.remove("hidden");
+      }
+    });
+
+    // Update prices
+    this.priceSlot = this.priceSlot || this.querySelector("[data-price-slot]");
+    this.compareAtPriceSlot =
+      this.compareAtPriceSlot ||
+      this.querySelector("[data-compare-at-price-slot]");
+
+    this.priceSlot.innerHTML = variantPrice;
+    if (this.compareAtPriceSlot) {
+      if (variantCompareAtPrice) {
+        this.compareAtPriceSlot.classList.remove("hidden");
+        this.compareAtPriceSlot.innerHTML = variantCompareAtPrice;
+      } else {
+        this.compareAtPriceSlot.classList.add("hidden");
+      }
+    }
+
+    // Toggle aria-pressed
+    const pressedButtons = this.querySelectorAll("[aria-pressed='true']");
+
+    pressedButtons.forEach((button) =>
+      button.setAttribute("aria-pressed", "false")
+    );
+
+    target.setAttribute("aria-pressed", "true");
+  }
 }
 
 ProductCard.register();
